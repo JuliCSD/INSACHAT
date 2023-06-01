5e55a8b5c089e0b9eb15b33c576fc3ae439552be
Chomosuke123 <122450828+Chomosuke123@users.noreply.github.com>
2023-06-01 16:17:51 +0200

    DossierBackend

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const { ObjectId } = require('mongodb');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/readDatabase', async (req, res) => {
+    const uri = "mongodb+srv://mathias:Tu07mLbgapte2C1d@cluster0.eauxg6l.mongodb.net/?retryWrites=true&w=majority"
+    const client = new MongoClient(uri);
+  
+    try {
+      await client.connect();
+      const database = client.db('insachat');
+      const collection = database.collection('users');
+  
+      const users = await collection.find().toArray();
+      
+      res.status(200).send(users);
+    } catch (error) {
+      console.error('An error occurred while attempting to connect to MongoDB', error);  // Log the error
+      res.status(500).send(error);
+    } finally {
+      await client.close();
+    }
+  });
+
+app.post('/addUser', async (req, res) => {
+  const user = req.body;
+
+  console.log('Received a request to add a user'); // Logs when a request is received
+
+  const uri = "mongodb+srv://mathias:Tu07mLbgapte2C1d@cluster0.eauxg6l.mongodb.net/?retryWrites=true&w=majority"
+  //const uri = 'mongodb+srv://mathias:Tu07mLbgapte2C1d@cluster0.eauxg6l.mongodb.net/';
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    const database = client.db('insachat');
+    const collection = database.collection('users');
+
+    const result = await collection.insertOne(user);
+    
+    res.status(200).send(`New user added with the following id: ${result.insertedId}`);
+  } catch (error) {
+    res.status(500).send(error);
+  } finally {
+    await client.close();
+  }
+});
+
+app.listen(5000, () => console.log('Server is running on port 5000'));
