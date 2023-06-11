8e2c3bf9e6749683fc54aa92f03f1d4df64d4ee0
nicoflux <nicolas.bloch1234@gmail.com>
2023-06-11 14:03:55 +0200

    CORRECTION cat.js

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -11,7 +11,7 @@ const CategoryFilter4 = () => {
   const [selectedSize, setSelectedSize] = useState('');
   const [selectedPrice, setSelectedPrice] = useState({ min: '', max: '' });
   const [isOpen, setIsOpen] = useState(false);
-  const [isLoading, setIsLoading] = useState(true); // Add isLoading state
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleMinPriceChange = (e) => {
     setSelectedPrice({ min: e.target.value, max: selectedPrice.max });
@@ -67,34 +67,6 @@ const CategoryFilter4 = () => {
     {
       price: 'price',
       name: 'Price',
-      options: [
-        {
-          value: selectedPrice.min,
-          type:'range',
-          label:
-            <input
-              type="range"
-              min={0}
-              max={100}
-              step={10}
-              value={selectedPrice.min}
-              onChange={handleMinPriceChange}
-          />
-        },
-        {
-          value: selectedPrice.max,
-          label: "tests"
-            /*<input
-              type="range"
-              min={0}
-              max={100}
-              step={10}
-              value={selectedPrice.max}
-              onChange={handleMaxPriceChange}
-            />*/
-        },
-
-      ],
       selected: selectedPrice,
       setSelected: setSelectedPrice,
     }
@@ -157,7 +129,7 @@ const CategoryFilter4 = () => {
                     name={filter.id}
                     id={filter.id}
                     defaultValue={''}
-                    className="bg-rose-50 border border-white-300 text-sm rounded-lg focus:ring-rose-500 block w-full p-2 focus:border-rose-500 focus:ring-rose-500 hover:scale-105 cursor-pointer ease-in-out duration-200"
+                    className="bg-rose-50 border border-white-300 text-sm rounded-lg block w-full p-2 focus:border-rose-500 focus:ring-rose-500 hover:scale-105 cursor-pointer ease-in-out duration-200"
                     onChange={(e) => {
                       filter.setSelected(e.target.value);
                     }}
@@ -174,36 +146,49 @@ const CategoryFilter4 = () => {
                 </div>
               )))}
               <div className="relative select-none" ref={dropdownRef}>
-        <div className={classNames("flex items-center justify-between bg-rose-50 border border-white-300 text-sm rounded-lg ${isOpen ? ring-rose-500 block w-full p-2 border-rose-500 ring-rose-500} hover:scale-105 cursor-pointer ease-in-out duration-200")} onClick={handleDropdownClick}>
+        <div className={"flex items-center justify-between p-2 bg-rose-50 border border-white-300 text-sm rounded-lg block w-full " + (isOpen ? 'border-rose-500 ring-rose-500' : '') + "hover:scale-105 cursor-pointer transition-transform ease-in-out duration-200" } onClick={handleDropdownClick}>
         <span className="mr-2">Price Range</span>
         <ChevronDownIcon className="-mr-1 h-4 w-4" aria-hidden="true" />
         </div>
         {isOpen && (
-          <div className="absolute z-10 bg-white border border-gray-300 p-4 mt-2">
+          <div className="absolute z-10 items-center justify-between bg-rose-50 border border-white-300 text-sm rounded-lg p-2">
             <div className="flex">
-              <label htmlFor="minPriceSlider">Min</label>
+              <label className="pr-2" htmlFor="minPriceSlider">Min</label>
               <input
+                className='pr-2'
                 type="range"
                 id="minPriceSlider"
                 min={0}
-                max={selectedPrice.max}
+                max={selectedPrice.max === '' ? 1000 : selectedPrice.max}
                 value={selectedPrice.min}
                 onChange={handleMinPriceChange}
               />
-              {selectedPrice.min}$
+              <div className='pl-2'>
+              {selectedPrice.min==='' ? '': selectedPrice.min+'€'}
+              </div>
             </div>
             <div className='flex'>
-              <label htmlFor="maxPriceSlider">Max</label>
+              <label className='pr-2' htmlFor="maxPriceSlider">Max</label>
               <input
                 type="range"
                 id="maxPriceSlider"
-                min={selectedPrice.min}
+                min={selectedPrice.min === '' ? 0 : selectedPrice.min}
                 max={1000}
                 value={selectedPrice.max}
                 onChange={handleMaxPriceChange}
               />
-              {selectedPrice.max}$
+              <div className='pl-2'>
+                {selectedPrice.max==='' ? '': selectedPrice.max+'€'}
+              </div>
             </div>
+            <button
+              className="mt-2 p-1 bg-white rounded text-m text-gray-500 hover:text-gray-700 cursor-pointer"
+              onClick={() => {
+                setSelectedPrice({min: '', max: ''});
+              }}
+            >
+              Default
+            </button>
           </div>
         )}
       </div>  
