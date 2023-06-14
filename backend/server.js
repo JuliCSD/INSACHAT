94e5934333fe76c4403aaffa80aafb724da36aa7
nicoflux <nicolas.bloch1234@gmail.com>
2023-06-14 19:36:54 +0200

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,26 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/readDatabase', async (req, res) => {
-    const uri = "mongodb+srv://mathias:Tu07mLbgapte2C1d@cluster0.eauxg6l.mongodb.net/?retryWrites=true&w=majority"
-    const client = new MongoClient(uri);
-  
-    try {
-      await client.connect();
-      const database = client.db('insachat');
-      const collection = database.collection('users');
-  
-      const users = await collection.find().toArray();
-      
-      res.status(200).send(users);
-    } catch (error) {
-      console.error('An error occurred while attempting to connect to MongoDB', error);  // Log the error
-      res.status(500).send(error);
-    } finally {
-      await client.close();
-    }
-  });
-
   app.get('/readAnnonces', async (req, res) => {
     const uri = "mongodb+srv://mathias:Tu07mLbgapte2C1d@cluster0.eauxg6l.mongodb.net/?retryWrites=true&w=majority"
     const client = new MongoClient(uri);
@@ -40,8 +20,27 @@ app.get('/readDatabase', async (req, res) => {
       const collection = database.collection('annonces');
   
       const annonces = await collection.find().toArray();
-      //console.log(annonces);
       
+      res.status(200).send(annonces);
+    } catch (error) {
+      console.error('An error occurred while attempting to connect to MongoDB', error);  // Log the error
+      res.status(500).send(error);
+    } finally {
+      await client.close();
+    }
+  });
+
+  app.get('/readAnnoncesPostees', verifyToken, async (req, res) => {
+    const uri = "mongodb+srv://mathias:Tu07mLbgapte2C1d@cluster0.eauxg6l.mongodb.net/?retryWrites=true&w=majority"
+    const client = new MongoClient(uri);
+  
+    try {
+      await client.connect();
+      const database = client.db('insachat');
+      const collection = database.collection('annonces');
+      owner=req.userId;
+
+      const annonces = await collection.find({owner : owner}).toArray();      
       res.status(200).send(annonces);
     } catch (error) {
       console.error('An error occurred while attempting to connect to MongoDB', error);  // Log the error
@@ -53,19 +52,14 @@ app.get('/readDatabase', async (req, res) => {
 
   app.get('/readProduct/:id', async (req, res) => {
     const uri = "mongodb+srv://mathias:Tu07mLbgapte2C1d@cluster0.eauxg6l.mongodb.net/?retryWrites=true&w=majority"
-    const client = new MongoClient(uri);
-  
-    //console.log(`Received request to get product with id: ${req.params.id}`);
-    
+    const client = new MongoClient(uri);    
     try {
       await client.connect();
       const database = client.db('insachat');
       const collection = database.collection('annonces');
       
       const query = { _id: new ObjectId(req.params.id) };  // Create a query with the hardcoded ID
-  
       const produit = await collection.findOne(query);
-      //console.log(`Found product in database: ${JSON.stringify(produit)}`);
       
       res.status(200).send(produit);
     } catch (error) {
@@ -76,14 +70,30 @@ app.get('/readDatabase', async (req, res) => {
     }
   });
   
+  app.get('/deleteProduct/:id', async (req, res) => {
+    const uri = "mongodb+srv://mathias:Tu07mLbgapte2C1d@cluster0.eauxg6l.mongodb.net/?retryWrites=true&w=majority"
+    const client = new MongoClient(uri);
+    
+    try {
+      await client.connect();
+      const database = client.db('insachat');
+      const collection = database.collection('annonces');
+      
+      const query = { _id: new ObjectId(req.params.id) };  // Create a query with the hardcoded ID
+  
+      const result = await collection.deleteOne(query);      
+      res.status(200).send(result);
+    } catch (error) {
+      console.error('An error occurred while attempting to connect to MongoDB', error);  // Log the error
+      res.status(500).send(error);
+    } finally {
+      await client.close();
+    }
+  });
 
 app.post('/addUser', async (req, res) => {
   const user = req.body;
-
-  //console.log('Received a request to add a user'); // Logs when a request is received
-
   const uri = "mongodb+srv://mathias:Tu07mLbgapte2C1d@cluster0.eauxg6l.mongodb.net/?retryWrites=true&w=majority"
-  //const uri = 'mongodb+srv://mathias:Tu07mLbgapte2C1d@cluster0.eauxg6l.mongodb.net/';
   const client = new MongoClient(uri);
 
   try {
@@ -104,13 +114,10 @@ app.post('/addUser', async (req, res) => {
 app.post('/login', async (req, res) => {
   const uri = "mongodb+srv://mathias:Tu07mLbgapte2C1d@cluster0.eauxg6l.mongodb.net/?retryWrites=true&w=majority"
   const client = new MongoClient(uri);
-  //console.log('Received a request to login a user');
-
   try {
       await client.connect();
       const database = client.db('insachat');
       const collection = database.collection('users');
-      //console.log(req.body.email);
       const user = await collection.findOne({ email: req.body.email });
     
       if (!user) {
@@ -125,10 +132,7 @@ app.post('/login', async (req, res) => {
 
       const token = jwt.sign({ id: user._id }, 'your_secret_key_here', {
           expiresIn: 7200 // expires in 24 hours
-      });
-
-      //console.log(`User ${user.email} logged in successfully`);
-    
+      });    
       res.status(200).send({ auth: true, token: token });
   } catch (error) {
       console.error('An error occurred while attempting to connect to MongoDB', error);
@@ -142,7 +146,7 @@ function verifyToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   //const token = req.headers['x-access-token'];
-  //console.log(token);
+
   if (!token)
       return res.status(403).send({ auth: false, message: 'No token provided.' });
 
@@ -152,7 +156,6 @@ function verifyToken(req, res, next) {
     
       // if everything good, save to request for use in other routes
       req.userId = decoded.id;
-      //console.log(`User id decoded : ${req.userId}`);
       next();
   });
 }
@@ -160,8 +163,6 @@ function verifyToken(req, res, next) {
 app.post('/addProduct', verifyToken, async (req, res) => {
   const product = req.body;
 
-  //console.log('Received a request to add a product'); // Logs when a request is received
-  //console.log("requesting name from",req.userId);
   product.owner = req.userId;
 
   const uri = "mongodb+srv://mathias:Tu07mLbgapte2C1d@cluster0.eauxg6l.mongodb.net/?retryWrites=true&w=majority"
@@ -196,34 +197,6 @@ app.get('/VerifyExpire', async (req, res) => {
   } else {
     res.status(200).send('not expired');
     //console.log("token not expired");
-  }
-});
-
-app.get('/getName', verifyToken, async (req, res) => {
-  
-  const uri = "mongodb+srv://mathias:Tu07mLbgapte2C1d@cluster0.eauxg6l.mongodb.net/?retryWrites=true&w=majority";
-  const client = new MongoClient(uri);
-
-  try {
-    await client.connect();
-    const database = client.db('insachat');
-    const collection = database.collection('users');
-
-    //console.log("requesting name from",req.userId);
-    const query = { _id: new ObjectId(req.userId) };  // Create a query with the hardcoded ID
-  
-    const user = await collection.findOne(query);
-    if (!user) {
-      return res.status(404).send('No user found.');
-    }
-
-    // Assume 'favorites' is an array of favorite items stored in the user document
-    res.status(200).send(user.nom);
-  } catch (error) {
-    console.error('An error occurred while attempting to connect to MongoDB', error);
-    res.status(500).send(error);
-  } finally {
-    await client.close();
   }
 });
 
